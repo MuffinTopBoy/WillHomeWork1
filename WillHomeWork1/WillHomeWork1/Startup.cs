bddba120581834b6af4e280039b469db6ac98a75
Wilson <Wilson_Wu@Compal.com>
2017-05-12 01:03:36 +0800

    Add CRUD Function

---
+++ WillHomeWork1/WillHomeWork1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WillHomeWork1.Startup))]
+namespace WillHomeWork1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}

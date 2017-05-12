c303b9b08ed3dc45fcaec8b86156999456a60be6
Wilson <Wilson_Wu@Compal.com>
2017-05-13 01:27:49 +0800

    剩兩個QQ
    V* 實作一個清單頁面，顯示欄位有「客戶名稱、聯絡人數量、銀行帳戶數量」共三個欄位，此功能要在資料庫中建立一個檢視表，並且匯入 EDMX。
    * 在列表頁要實作「搜尋」功能
    * 實作「客戶聯絡人」時，同一個客戶下的聯絡人，其 Email 不能重複。

--- WillHomeWork1/WillHomeWork1/App_Start/RouteConfig.cs
+++ WillHomeWork1/WillHomeWork1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WillHomeWork1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "客戶清單", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ WillHomeWork1/WillHomeWork1/Controllers/客戶清單Controller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WillHomeWork1.Models;
+
+namespace WillHomeWork1.Controllers
+{
+    public class 客戶清單Controller : Controller
+    {
+        客戶資料Entities db = new 客戶資料Entities();
+        // GET: 客戶清單
+        public ActionResult Index()
+        {
+            var data = db.客戶清單.ToList();
+            return View(data);
+        }
+    }
+}
